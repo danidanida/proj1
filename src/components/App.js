df448f5dda0fcff9583059432ffa9413d5107cad
Bahdana B <bahdana.babkova@gmail.com>
2021-06-02 16:21:57 +0400

    feat: add api, add json server, mock db, redux, thunk

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
@@ -6,13 +6,12 @@ import CoursesPage from "./courses/CoursesPage";
 import Header from "./common/Header";
 import PageNotFound from "./PageNotFound";
 
-// exact is only for "/" route
 function App() {
   return (
     <div className="container-fluid">
       <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} /> 
+        <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/courses" component={CoursesPage} />
         <Route component={PageNotFound} />
--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,9 +1,76 @@
 import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/CourseActions";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
 
 class CoursesPage extends React.Component {
+  /* constructor(props) {
+    super(props);
+
+    this.state = {
+      course: {
+        title: "",
+      },
+    };
+    this.handleChange = this.handleChange.bind(this); 
+  }
+
+  handleChange(event) {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course: course });
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    //this.props.dispatch(courseActions.createCourse(this.state.course));
+    //this.props.createCourse(this.state.course);
+    this.props.actions.createCourse(this.state.course);
+  };*/
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      /*<form onSubmit={this.handleSubmit}>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+    <input type="submit" value="Save" />
+          </form>
+  */
+      <>
+        <h2>Courses</h2>
+        {this.props.courses.map((course) => (
+          <div key={course.title}>{course.title}</div>
+        ))}
+      </>
+    );
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired,
+  //createCourse: PropTypes.func.isRequired,
+  //dispatch: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    //createCourse: (course) => dispatch(courseActions.createCourse(course)),
+    actions: bindActionCreators(courseActions, dispatch),
+  };
+}
+
+/*const mapDispatchToProps = { // mapDispatchToProps as an object
+  createCourse: courseActions.createCourse
+}*/
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
